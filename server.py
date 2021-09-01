f3a7c379ce681efe4016adfb06c26c277dde1f32
shobhith K <shobhithkamata01@gmail.com>
2021-09-02 02:55:21 +0530

    wrote basic server

--- server.py
+++ server.py
@@ -1,29 +1,47 @@
-# using flask_restful
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
+from flask import Flask, request, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
+
 
 app = Flask(__name__)
-api = Api(app)
+app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.sqlite3'
+db = SQLAlchemy(app)
 
 
-class Hello(Resource):
+class data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String(100))
+    text = db.Column(db.String(1000))
 
-    def get(self):
-        return jsonify({'message': 'hello world'})
-
-    def post(self):
-        data = request.get_json()  # status code
-        return jsonify({'data': data}), 201
-
-
-class Square(Resource):
-
-    def get(self, num):
-        return jsonify({'square': num ** 2})
+    def __init__(self, uid, text):
+        self.uid = uid
+        self.text = text
 
 
-api.add_resource(Hello, '/')
-api.add_resource(Square, '/square/<int:num>')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        text = request.form['name']
+        uid = str(uuid4())
+        newData = data(uid, text)
+        db.session.add(newData)
+        db.session.commit()
+        return redirect(url_for("user", usr=uid))
+    else:
+        return render_template('index.html')
+
+
+@app.route("/<usr>")
+def user(usr):
+    found_user = data.query.filter_by(id=usr).first()
+    return f'<h1>{found_user.text}</h1>'
+
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
+
+
+
+
