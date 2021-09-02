f4f9b9f776f2af2c0c588cc2cd2db20686af3cce
suprajam06 <89857007+suprajam06@users.noreply.github.com>
2021-09-02 15:26:46 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,44 +3,52 @@ from flask_sqlalchemy import SQLAlchemy
 from uuid import uuid4
 
 
+
 app = Flask(__name__)
 app.secret_key = "hello"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./sqlite3'
 db = SQLAlchemy(app)
 
 
-class data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uid = db.Column(db.String(100))
-    text = db.Column(db.String(1000))
+class Data(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    uid=db.Column(db.String(1000))
+    text= db.Column(db.String(1000))
+
 
     def __init__(self, uid, text):
         self.uid = uid
         self.text = text
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == "POST":
-        text = request.form['name']
-        uid = str(uuid4())
-        newData = data(uid, text)
-        db.session.add(newData)
-        db.session.commit()
-        return redirect(url_for("user", usr=uid))
-    else:
-        return render_template('index.html')
+
+@app.route('/')
+def text():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def text_post():
+    text = request.form['name']
+    uid = str(uuid4())
+    newData = Data(uid, text)
+    db.session.add(newData)
+    db.session.commit()
+    processed_text = text
+
+    return f'<h1>{processed_text}</h1>'
 
 
-@app.route("/<usr>")
-def user(usr):
-    found_user = data.query.filter_by(uid = usr).first()
-    return f'<h1>{found_user.text}</h1>'
 
+@app.route("/view")
+def view():
+    return render_template('view.html',values=Data.query.all())
 
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
+
+
+
 
 
 
